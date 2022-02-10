de647c9648b17e3c649d88db05470d2b518fb6bc
RomanL88 <95365068+RomanL88@users.noreply.github.com>
2022-02-10 11:51:04 +0500

    теперь 3-е задание сделано
    закончил последний пунктик:
    А в конце выведите на экран итоговую строку:
    "Итого - <Сумма стоимостей всех игрушек>"

--- oop-in-php/toyFactory/ToyParty.php
+++ oop-in-php/toyFactory/ToyParty.php
@@ -15,7 +15,10 @@ for ($i = 0; $i < $countToys; $i++) {
     //случайное имя игрушки
     $randToyName = $ToyNameParty[rand(0, 4)];
     $priceToys = rand(30, 1000);
+    $priceToysAll += $priceToys;
 
     $ToyParty = new Toy($randToyName, $priceToys);
     $ToyParty->createToy($randToyName);
 }
+echo "<br>";
+echo "Итого - $priceToysAll";
